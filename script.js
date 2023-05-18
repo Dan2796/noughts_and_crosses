e770e412746890faf1516d8eca75599f49fdfee7
Dan Snow <danjsnow@outlook.com>
2023-05-18 11:22:30 +0100

    Adds check win condition and alternating turns

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ const displayController = (() => {
       console.log(`${board[row][0]} | ${board[row][1]} | ${board[row][2]}`);
     }
   };
-  const playerWins = (player) => {
-    console.log(`${player} wins this round!`);
+  const playerWins = (xToMove) => {
+    // Note that if X is to move then X has just lost because O must have just moved
+    console.log(`${xToMove ? 'Player O' : 'Player X'} wins this round!`);
   };
   return { refreshBoard, playerWins };
 })();
@@ -21,35 +22,55 @@ const displayController = (() => {
 const gameboard = (() => {
   let board;
   let gameOver;
+  let xToMove;
   const getBoard = () => board;
-  const resetBoard = () => {
+  const resetGame = () => {
     board = [
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
     ];
     gameOver = false;
+    xToMove = true;
   };
-  resetBoard();
-  const addMark = (row, col, playerIsX) => {
-    const mark = playerIsX ? 1 : -1;
-    if (gameOver) { return; };
-    if (row < 0 || row > 2 || col < 0 || col > 2) { return; };
-    if (board[row][col] !== 0) { return; };
+  resetGame();
+  const addMark = (row, col) => {
+    const mark = xToMove ? 1 : -1;
     board[row][col] = mark;
     displayController.refreshBoard(board);
+    xToMove = !xToMove;
   };
+  const checkArraySumsTarget = (array, target) => {
+    const arraySum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return arraySum === target;
+  };
+  /* It would probably be more efficient to only check rows and cols based on the latest input.
+     For example, don't check the top row if a bottom mark has just been made. But the performance
+     differences are small, and this code is easier to write. */
   const checkWin = () => {
+    // Only check zeros or xs depending on which player may have just won:
+    const targetSum = xToMove ? -3 : 3;
+    return (checkArraySumsTarget(board[0], targetSum)
+            || checkArraySumsTarget(board[1], targetSum)
+            || checkArraySumsTarget(board[2], targetSum)
+            || checkArraySumsTarget([board[0][0], board[1][0], board[2][0]], targetSum)
+            || checkArraySumsTarget([board[0][1], board[1][1], board[2][1]], targetSum)
+            || checkArraySumsTarget([board[0][2], board[1][2], board[2][2]], targetSum)
+            || checkArraySumsTarget([board[0][0], board[1][1], board[2][2]], targetSum)
+            || checkArraySumsTarget([board[0][2], board[1][1], board[2][0]], targetSum));
   };
-  const markAndCheck = (row, col, player) => {
-    addMark(row, col, player.isX);
+  const markAndCheck = (row, col) => {
+    // check game running and move is valid:
+    if (gameOver) { return; }
+    if (row < 0 || row > 2 || col < 0 || col > 2) { return; }
+    if (board[row][col] !== 0) { return; }
+    addMark(row, col);
     if (checkWin()) {
       gameOver = true;
-      player.addWin();
-      displayController.playerWins(player);
+      displayController.playerWins(xToMove);
     }
   };
-  return { getBoard, resetBoard, addMark };
+  return { getBoard, resetGame, markAndCheck };
 })();
 
 displayController.refreshBoard(gameboard.getBoard());
